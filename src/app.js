22ac360bc25eabe21caedcdaff724dad50e0afb8
nghiem102 <ledinhnghiem102@gmail.com>
2022-03-29 15:36:54 +0700

    swager

--- src/app.js
+++ src/app.js
@@ -2,12 +2,15 @@ import express from "express";
 import mongoose from "mongoose";
 import morgan from  "morgan";
 import cors from 'cors'
+import swaggerUi from 'swagger-ui-express'
+import YAML from 'yamljs'
 
 import productRoute from './routers/product'
 import categoryRoute from './routers/category'
 import userRoute from './routers/user'
 
 const app = express()
+const swaggerJSDocs = YAML.load('./api.yaml')
 
 app.use(express.json());
 app.use(morgan("tiny"));
@@ -16,6 +19,8 @@ app.use(cors())
 app.use("/api",productRoute)
 app.use("/api",categoryRoute)
 app.use("/api",userRoute)
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerJSDocs))
 
 
 mongoose.connect('mongodb://localhost:27017/test')
